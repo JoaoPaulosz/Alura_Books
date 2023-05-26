139675037c4b54180af7b54555ba0540d973c13d
João Souza <joao.souza@asv.com.br>
2023-05-26 17:20:55 -0300

    Aplicado metodo sort e inciado filtragem de disponibilidade

--- app/main.js
+++ app/main.js
@@ -3,6 +3,6 @@ let livros = buscaLivros()
 async function buscaLivros(){
     let requisicaoLivros = await fetch('https://guilhermeonrails.github.io/casadocodigo/livros.json')
     livros = await requisicaoLivros.json();
-    let livrosComDesconto = aplicaDesconto(livros)
-    exibirLivrosNaTela(livrosComDesconto)
+    livros = aplicaDesconto(livros)
+    exibirLivrosNaTela(livros)
 }
